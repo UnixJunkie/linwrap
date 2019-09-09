ad7ee0b3fc14de633e04bfbfac3ab9343dbecd7a
Francois Berenger <unixjunkie@sdf.org>
2019-09-09 17:42:33 +0900

    Parmap_wrapper: minimalist code

--- src/parmap_wrapper.ml
+++ src/parmap_wrapper.ml
@@ -2,16 +2,15 @@
 (* parmap-like wrapper using parany *)
 let parmap ~ncores ~csize ~f l =
   let input = ref l in
-  let demux () =
-    match !input with
+  let demux () = match !input with
     | [] -> raise Parany.End_of_input
-    | x :: xs ->
-      let res = x in
-      input := xs;
-      res in
+    | x :: xs -> (input := xs; x) in
   let output = ref [] in
   let mux x =
-    output := x :: !output
-  in
+    output := x :: !output in
+  (* for safety *)
+  Parany.set_copy_on_work ();
+  Parany.set_copy_on_mux ();
+  (* parallel work *)
   Parany.run ~verbose:false ~csize ~nprocs:ncores ~demux ~work:f ~mux;
   !output
