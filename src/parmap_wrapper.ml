bb0b2b6edc4fec1fcc96fdb7da421eb410b0a61a
Francois Berenger <unixjunkie@sdf.org>
2019-09-09 17:46:14 +0900

    Parmap_wrapper: don't invoke parany in vain

--- src/parmap_wrapper.ml
+++ src/parmap_wrapper.ml
@@ -1,16 +1,18 @@
 
 (* parmap-like wrapper using parany *)
 let parmap ~ncores ~csize ~f l =
-  let input = ref l in
-  let demux () = match !input with
-    | [] -> raise Parany.End_of_input
-    | x :: xs -> (input := xs; x) in
-  let output = ref [] in
-  let mux x =
-    output := x :: !output in
-  (* for safety *)
-  Parany.set_copy_on_work ();
-  Parany.set_copy_on_mux ();
-  (* parallel work *)
-  Parany.run ~verbose:false ~csize ~nprocs:ncores ~demux ~work:f ~mux;
-  !output
+  if ncores <= 1 then BatList.map f l
+  else
+    let input = ref l in
+    let demux () = match !input with
+      | [] -> raise Parany.End_of_input
+      | x :: xs -> (input := xs; x) in
+    let output = ref [] in
+    let mux x =
+      output := x :: !output in
+    (* for safety *)
+    Parany.set_copy_on_work ();
+    Parany.set_copy_on_mux ();
+    (* parallel work *)
+    Parany.run ~verbose:false ~csize ~nprocs:ncores ~demux ~work:f ~mux;
+    !output
