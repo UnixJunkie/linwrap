8a4525718104e38381f00827bf697b8ef217f965
Francois Berenger <unixjunkie@sdf.org>
2020-04-03 15:49:59 +0900

    ecfp6.py: work towards parallelization

--- bin/ecfp6.py
+++ bin/ecfp6.py
@@ -8,18 +8,11 @@
 # Kyushu Institute of Technology,
 # 680-4 Kawazu, Iizuka, Fukuoka, 820-8502, Japan.
 
-import sys, rdkit, time
+import multiprocessing, sys, rdkit, time
 from rdkit import Chem
 from rdkit.Chem import AllChem
+from multiprocessing import Pool
 from optparse import OptionParser
-
-def RobustSmilesMolSupplier(filename):
-    with open(filename) as f:
-        for line in f:
-            words = line.split()
-            smile = words[0]
-            name = " ".join(words[1:]) # everything after the SMILES
-            yield (Chem.MolFromSmiles(smile), name)
 
 # CLI setup -------------------------------------------------------------------
 parser = OptionParser(usage = """Usage: %prog -i in.smi \
@@ -27,6 +20,10 @@ parser = OptionParser(usage = """Usage: %prog -i in.smi \
 parser.add_option("-i", action = "store", type = "string", dest = "in_fn")
 parser.add_option("-b", action = "store", type = "int", dest = "nb_bits",
                   default = 2048) # rdkit's default
+parser.add_option("-n", action = "store", type = "int", dest = "nprocs",
+                  default = 1)
+parser.add_option("-c", action = "store", type = "int", dest = "csize",
+                  default = 1000)
 parser.add_option("-r", action = "store", type = "int", dest = "radius",
                   default = 3) # ECFP6
 # output in sparse format
@@ -56,7 +53,6 @@ def read_several(count, opened_file):
     for i in range(count):
         line = opened_file.readline()
         if line == '':
-            opened_file.close()
             break
         words = line.split()
         smile = words[0]
@@ -75,31 +71,45 @@ if __name__ == '__main__':
         exit(0)
     input_fn = opts.in_fn
     nb_bits = opts.nb_bits
+    nprocs = opts.nprocs
+    csize = opts.csize
     assert(nb_bits == 1024 or nb_bits == 2048 or nb_bits == 16384)
     fp_radius = opts.radius
     assert(fp_radius == 2 or fp_radius == 3)
     sparse_output = opts.sparse_fmt
     ok_count = 0
     ko_count = 0
-    for mol, name in RobustSmilesMolSupplier(input_fn):
-        if mol:
-            ok_count += 1
-            if (ok_count % 1000) == 0:
-                # user feedback
-                print("done: %d" % ok_count, end='\r',
-                      file=sys.stderr, flush=True)
-            # ECFP6 2048 bits
-            fp = AllChem.GetMorganFingerprintAsBitVect(mol, fp_radius,
-                                                       nBits = nb_bits)
-            bits = fp.ToBitString()
-            if sparse_output:
-                # liblinear-ready format
-                print("%s" % decode_mol(name, bits))
-            else:
-                # format for molenc's pubchem_decoder
-                print("%s,0.0,%s" % (name, bits))
+    workers_pool = Pool(nprocs)
+    opened_file = open(input_fn, 'r')
+
+    # WARNING: this function is defined here in order to use some
+    # "global" variables, because there is no partial function application
+    # in Python...
+    def process_one(x):
+        mol, name = x # unpack params
+        # ECFP fingerprint
+        fp = AllChem.GetMorganFingerprintAsBitVect(mol, fp_radius,
+                                                   nBits = nb_bits)
+        bits = fp.ToBitString()
+        if sparse_output:
+            # liblinear-ready format
+            return(decode_mol(name, bits))
         else:
-            ko_count += 1
+            # format for molenc's pubchem_decoder
+            return("%s,0.0,%s" % (name, bits))
+
+    while True:
+        mol_bunch = read_several(csize, opened_file)
+        nb_read = len(mol_bunch)
+        if nb_read == 0:
+            break # reached EOF
+        for x in mol_bunch:
+            print(process_one(x))
+        ok_count += nb_read
+        # user feedback
+        print("done: %d" % ok_count, end='\r', file=sys.stderr, flush=True)
+
+    opened_file.close()
     after = time.time()
     dt = after - before
     total = ok_count + ko_count
