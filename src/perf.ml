875c5ad3ab9c9410e4512648107f332baeeea5b7
Francois Berenger <unixjunkie@sdf.org>
2020-10-02 14:40:58 +0900

    Perf: faster calculation and write out of the ROC curve
    the PR curve has been dropped
    (too ugly looking, and rather costly to compute on large datasets)

--- src/perf.ml
+++ src/perf.ml
@@ -8,9 +8,9 @@
 
 open Printf
 
+module A = BatArray
 module Fn = Filename
 module L = BatList
-module Log = Dolog.Log
 
 let compose f g x =
   f (g x)
@@ -52,16 +52,42 @@ module Make (SL: Cpm.MakeROC.SCORE_LABEL) = struct
     assert(empty = []);
     (nb_actives, nb_decoys, L.rev rev_res)
 
+  let actives_portion_plot_a score_labels =
+    (* because thresholds are normalized *)
+    let thresholds = L.frange 0.0 `To 1.0 51 in
+    let nb_actives = Utls.array_count SL.get_label score_labels in
+    let nb_decoys = (A.length score_labels) - nb_actives in
+    let rev_res =
+      L.fold_left (fun acc t ->
+          let n =
+            Utls.array_count
+              (fun sl -> SL.get_score sl > t)
+              score_labels in
+          let card_act =
+            Utls.array_count
+              (fun sl -> (SL.get_label sl) && (SL.get_score sl > t))
+              score_labels in
+          let card_dec = n - card_act in
+          let ef =
+            if card_act = 0 || n = 0 then
+              0.0 (* there are no more actives above this threshold:
+                     the EF falls down to 0.0 (threshold too high) *)
+            else (* regular EF formula *)
+              (float card_act) /. (float n) in
+          let rem_acts = (float card_act) /. (float nb_actives) in
+          let rem_decs = (float card_dec) /. (float nb_decoys) in
+          (t, ef, rem_acts, rem_decs) :: acc
+        ) [] thresholds in
+    (nb_actives, nb_decoys, L.rev rev_res)
+
   let evaluate_performance ?noplot:(noplot = false)
-      top_n maybe_curve_fn scores_fn title_str score_labels =
+      top_n maybe_curve_fn scores_fn title_str score_labels_a =
     let for_auc = match top_n with
-      | None -> score_labels
-      | Some n ->
-        let topn = L.take n score_labels in
-        assert(L.length topn = n);
-        topn in
+      | None -> score_labels_a
+      | Some _n ->
+        failwith "Perf.evaluate_performance: top_n not supported" in
     (* save ROC curve *)
-    Utls.list_to_file scores_fn
+    Utls.array_to_file scores_fn
       (fun sl ->
          let score = SL.get_score sl in
          let label = SL.get_label sl in
@@ -71,19 +97,17 @@ module Make (SL: Cpm.MakeROC.SCORE_LABEL) = struct
     let curve_fn = match maybe_curve_fn with
       | None -> Fn.temp_file ~temp_dir:"/tmp" "rf_train_" ".roc"
       | Some fn -> fn in
-    let pr_curve_fn = Fn.temp_file ~temp_dir:"/tmp" "rf_train_" ".pr" in
-    let roc_curve = ROC.roc_curve for_auc in
-    let pr_curve = ROC.pr_curve for_auc in
-    Utls.list_to_file curve_fn (fun (x, y) -> sprintf "%f %f" x y) roc_curve;
-    Utls.list_to_file pr_curve_fn (fun (x, y) -> sprintf "%f %f" x y) pr_curve;
+    let roc_curve_a = ROC.fast_roc_curve_a for_auc in
+    Utls.array_to_file curve_fn (fun (x, y) -> sprintf "%f %f" x y)
+      roc_curve_a;
     (* plot ROC curve *)
     let ef_curve_fn = Fn.temp_file ~temp_dir:"/tmp" "rf_train_" ".ef" in
-    let nb_acts, nb_decs, ef_curve = actives_portion_plot for_auc in
+    let nb_acts, nb_decs, ef_curve = actives_portion_plot_a for_auc in
     Utls.list_to_file ef_curve_fn
       (fun (t, ef, ra, rd) -> sprintf "%f %f %f %f" t ef ra rd) ef_curve;
     (if not noplot then
        Gnuplot.roc_curve title_str
-         scores_fn curve_fn pr_curve_fn nb_acts nb_decs ef_curve_fn
+         scores_fn curve_fn nb_acts nb_decs ef_curve_fn
     );
 
 end
